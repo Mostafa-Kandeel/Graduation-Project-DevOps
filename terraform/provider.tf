48bd9a6134e6168642c54b2f6af88a147f9214b8
mostafa.kandeel <kandeelmostafa99@gmail.com>
2025-12-08 21:52:40 +0200

    changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 }
-
-# Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
 }
